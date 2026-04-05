65f83a5b6c65300fe9618d239e0941fdc9f13c68
gilbert3010 <gilbert.meneses30@gmail.com>
2026-04-05 14:35:14 -0400

    practica-python

--- practica_poo2.py
+++ practica_poo2.py
@@ -6,23 +6,52 @@ class Personaje:
         self.defensa = defensa
         
     def ingresar_nombre(self):
-        self.nombre = input("Ingrese el nombre de su personaje: ")
+        self.nombre = input(f"Ingrese el nombre de su personaje: ")
         
     def mostrar_atributos(self, nombre, vida, ataque, defensa):
         print(f"Nombre: {nombre}")
         print(f"Vida: {vida}")
         print(f"Ataque: {ataque}")
         print(f"Defensa: {defensa}")
+    
+class Guerrero(Personaje):
+    def __init__(self, nombre=None, vida=100, ataque=10, defensa=5, arma="30", armadura="20"):
+        super().__init__(nombre, vida, ataque, defensa)
+        self.arma = arma
+        self.armadura = armadura
+    def ingresar_nombre(self):
+        self.nombre = input(f"Ingrese el nombre de su Guerrero: ")
 
-mi_personaje = Personaje()
+    def atributos(self, arma, armadura):
+        ataque_total = self.ataque + int(arma)
+        defensa_total = self.defensa + int(armadura)
+        super().mostrar_atributos(self.nombre, self.vida, self.ataque, self.defensa)
+        print(f"Ataque total: {ataque_total}")
+        print(f"Defensa total: {defensa_total}")
         
-while True:
-    opcion = input("Elija una opcion: \n1. Ingresar nombre de Personaje. \n2. Mostrar Atributos. \n3. Salir.")
-    if opcion == "1":
-        mi_personaje.ingresar_nombre()
-    elif opcion == "2":
-        mi_personaje.mostrar_atributos(mi_personaje.nombre, mi_personaje.vida, mi_personaje.ataque, mi_personaje.defensa)
-    elif opcion == "3":
-        print("Saliendo...")
-        break
-            
+class Mago(Personaje):
+    def __init__(self, nombre=None, vida=100, ataque=10, defensa=5, arma_magica="40", vestido_magico="10"):
+        super().__init__(nombre, vida, ataque, defensa)
+        self.arma_magica = arma_magica
+        self.vestido_magico = vestido_magico
+    
+    def ingresar_nombre(self):
+        self.nombre = input(f"Ingrese el nombre de su Mago: ")
+
+    def atributos(self, arma_magica, vestido_magico):
+        ataque_total = self.ataque + int(arma_magica)
+        defensa_total = self.defensa + int(vestido_magico)
+        super().mostrar_atributos(self.nombre, self.vida, self.ataque, self.defensa)
+        print(f"Ataque total: {ataque_total}")
+        print(f"Defensa total: {defensa_total}")
+        
+
+#mi_personaje = Personaje(Guerrero)
+
+guerrero = Guerrero()
+mago = Mago()
+
+guerrero.ingresar_nombre()
+guerrero.atributos(guerrero.arma, guerrero.armadura)
+mago.ingresar_nombre()
+mago.atributos(mago.arma_magica, mago.vestido_magico)
