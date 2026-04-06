2088a8b05bc95422cf00ee6b414aa57194223ba1
gilbert3010 <gilbert.meneses30@gmail.com>
2026-04-06 18:22:58 -0400

    practica-python

--- archivo_py/archivos.py
+++ archivo_py/archivos.py
@@ -1,5 +1,6 @@
 class Venta:
     def __init__(self):
+        self.producto = []
         self.nombre_producto = None
         self.cantidad = None
         self.precio = 0
@@ -10,13 +11,28 @@ class Venta:
             self.nombre_producto = input("Nombre del producto: ")
             self.cantidad = int(input("Cantidad: "))
             self.precio = float(input("Precio: "))
-            ventas.write(f"{self.nombre_producto}, {self.cantidad}, {self.precio}\n")
+            ventas.write(f"Nombre: {self.nombre_producto}\n Cantidad: {self.cantidad}\n Precio: {self.precio}\n")
                 
     def mostrar_productos(self):
         with open("ventas.txt", "r") as ventas:
             for linea in ventas:
                 print(linea.strip())
-                    
+                
+    """"def comprar_producto(self, cantidad):
+        with open("ventas.txt", "r") as ventas:
+            print("Elige el producto que desea comprar: ")
+            for i, self.nombre_producto in enumerate(ventas.readlines()):
+                print(f"{i+1}. {self.nombre_producto}")
+            
+                if cantidad > self.cantidad:
+                    print("Sobre límite de productos disponibles.")
+                elif cantidad <= self.cantidad:
+                    self.cantidad -= cantidad
+                    print(f"Compra realizada. Quedan {self.cantidad} de {self.nombre_producto}productos dispobibles.")
+                else:
+                    print("Cantidad no válida.")"""
+        
+            
 venta = Venta()
 
 while True:
