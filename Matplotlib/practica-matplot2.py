6f407d5d4ba4e0060945c2fbbf9a2ab007d0c5a4
gilbert3010 <gilbert.meneses30@gmail.com>
2026-04-14 17:41:28 -0400

    practica-python-matplotlib3

--- Matplotlib/practica-matplot2.py
+++ Matplotlib/practica-matplot2.py
@@ -1,13 +1,27 @@
-import matplotlib.pyplot as mat
+import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.patheffects as patheffects
 
-finanzas = pd.read_csv("C:\\Users\\DK_GILBERT\\Desktop\\Práctica Python\\Matplotlib\\ejemplo_mat.csv")
+# Leer el archivo CSV con el separador correcto
+finanzas = pd.read_csv("C:\\Users\\DK_GILBERT\\Desktop\\Práctica Python\\Matplotlib\\ejemplo_mat.csv", sep=';')
+
+# Eliminar filas con valores vacíos
+finanzas = finanzas.dropna()
+
 print(finanzas['Elon Mosk'].head())
 
-fig, ax = mat.subplots(figsize=(15,8))
-ax.plot(finanzas['Elon Mosk'], label='Elon Mosk', lw=2, color='red',
-        path_effects=[mat.simplelineshadow(), mat.Normal()])
+fig, ax = plt.subplots(figsize=(15, 8))
 
+ax.plot(finanzas['Elon Mosk'], 
+        label='Elon Mosk', 
+        lw=2, 
+        color='red',
+        path_effects=[patheffects.SimpleLineShadow(), patheffects.Normal()])
 
-mat.show();
+ax.set_xlabel('Índice')
+ax.set_ylabel('Valor')
+ax.set_title('Evolución de Elon Mosk')
+ax.legend()
+ax.grid(True, alpha=0.3)
 
+plt.show()
