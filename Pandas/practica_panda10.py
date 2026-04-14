b5bdef4c2cf3a07b3f0dc9446a6b94f302798c3b
gilbert3010 <gilbert.meneses30@gmail.com>
2026-04-14 15:08:29 -0400

    practica-python-pandas3

--- Pandas/practica_panda10.py
+++ Pandas/practica_panda10.py
@@ -2,4 +2,22 @@ import pandas as pd
 
 csv = pd.read_csv("C:\\Users\\DK_GILBERT\\Desktop\\Práctica Python\\Pandas\\dataframe.csv")
 
-print(csv.groupby(['edad','nombre', 'apellido', 'nacionalidad']).agg(['min', 'max', 'count']))
+"""
+MÉTODO 'AGGGREGATE' (AGG)
+
+El método 'agg' permite aplicar multiples funciones de agregación a un 
+dataframe o a una serie. Es util para resumir y analizar datos de 
+manera eficiente. Puedes aplicar funciones cómo: 'min', 'max', 'count'.
+"""
+
+#print(csv.groupby(['edad','nombre', 'apellido', 'nacionalidad']).agg(['min', 'max', 'count']))
+
+filtrar = csv[((csv.nacionalidad == "venezolana") | (csv.nacionalidad == "ruso")) & (csv.edad > 29)]
+
+print(filtrar)
+
+ejemplo_groupby = csv.groupby("nacionalidad").size() #El método 'size()' es clave, te ahorra directamente el 'list' para mostrar resultados de agrupacion
+print(ejemplo_groupby)
+
+unstack = csv.groupby(['nacionalidad', 'edad']).size().unstack()
+print(unstack) # El método 'unstack' permite transformar un nivel de índice en columnas, lo que facilita la visualización de los datos agrupados.
