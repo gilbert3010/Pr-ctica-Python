76b9b6fb25939f83bcaa6f7c88b2f5302402938e
gilbert3010 <gilbert.meneses30@gmail.com>
2026-04-15 20:28:09 -0400

    analisis-datos

---
+++ analisis_datos.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+"""Crea un sistema que permita analizar los datos de un archivo CSV.
+El sistema debe permitir al usuario seleccionar el archivo CSV que desea analizar."""
+
+# Paso 1: Crear la clase AnalisisDatos.
+# Paso 2: Crear un método para cargar el archivo CSV.
+# Paso 3: 
+
+class AnalisisDatos:
+    def __init__(self, archivo, datos):
+        self.archivo = archivo
+        self.datos = datos
+        
+    def cargar_datos(self):
+        self.archivo = input("Ingrese el nombre del archivo CSV que desea analizar:")
+        self.datos = pd.read_excel(self.archivo)
+            
+            
+datos = AnalisisDatos(archivo='', datos='')
+datos.cargar_datos()
+            
+        
